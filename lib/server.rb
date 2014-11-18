00be940ccca67a2892a4c12d566d9b09b06749ab
danielobembe <ayo.obembe@gmail.com>
2014-11-18 15:44:25 +0000

    adding user accounts, password handling

--- lib/server.rb
+++ lib/server.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require './lib/link'
 require 'data_mapper'
 require './lib/tag'
+require './lib/user'
 
 	set :views, Proc.new{File.join(root,'..','views')}
   env = ENV["RACK_ENV"] || "development"
@@ -9,6 +10,9 @@ require './lib/tag'
   DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
+
+  enable :sessions
+  set :session_secret, 'super secret'
 
 	get '/' do
 	  @links = Link.all
@@ -31,5 +35,21 @@ require './lib/tag'
 		erb :index
 	end
 
+	get '/users/new' do 
+		erb :"users/new"
+	end
+
+	post '/users' do 
+		user = User.create(:email => params[:email],
+								:password => params[:password])
+		session[:user_id] = user.id 
+		redirect('/')
+	end
+
 	
+	helpers do 
+		def current_user
+			@current_user ||= User.get(session[:user_id]) if session[:user_id]
+		end
+	end
 
