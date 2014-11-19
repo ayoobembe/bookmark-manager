e870ed757a9a658fa2caaee31d609bd1b48e9973
danielobembe <ayo.obembe@gmail.com>
2014-11-19 09:22:54 +0000

    Completed  -preventing duplicate registration - in -adding user accounts-

--- lib/server.rb
+++ lib/server.rb
@@ -53,7 +53,8 @@ require 'rack-flash'
 			session[:user_id] = @user.id 
 			redirect to('/')
 		else
-			flash[:notice]="Sorry,your passwords don't match"
+			# flash[:notice]="Sorry,your passwords don't match"
+			flash.now[:errors]=@user.errors.full_messages
 			erb :"users/new"
 		end
 	end
